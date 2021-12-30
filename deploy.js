db29ca503f3fae84d33f9f77044ca7459a6d2349
sachukk <kiryl.sachuk@instinctools.com>
2021-12-30 16:53:32 +0300

    initial commit

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "swing only glory affair slam ramp quick legend empower cupboard secret boil",
+  // remember to change this to your own phrase!
+  "https://rinkeby.infura.io/v3/47e9ff9c060243f5aa397b484d6ae5a1"
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
